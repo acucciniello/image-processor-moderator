0b016048aa62d11c3753b741c3c06f72c7a0fc33
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-05-03 13:40:57 -0400

    Now downloads an image locally from s3
        Next Step is to use face analysis to get faces and center point of
        image

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,6 @@
+var sqs = require('./index.js')
 
+exports.handler = function(event, context) {
+  
+  context.succeed("Hello, World!")
+}
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var AWS = require('aws-sdk')
 AWS.config.loadFromPath('./aws.credentials.json')
 
 var sqs = new AWS.SQS({apiVersion: '2012-11-05'})
+var s3 = new AWS.S3({apiVersion: '2006-03-01'})
 
 var queueURL = 'https://sqs.us-east-1.amazonaws.com/021919146663/image-processor-input'
 
-var params = {
+var sqsParams = {
   AttributeNames: [
     'SentTimestamp'
   ],
@@ -18,11 +19,28 @@ var params = {
   WaitTimeSeconds: 0
 }
 
-sqs.receiveMessage(params, function(err, data) {
+/*var s3Params = {
+  Bucket: 'image-processor-moderator',
+  // CopySource: 'image-processor-moderator/source/person1.jpg',
+  Key: 'source/person1.jpg'
+}*/
+
+sqs.receiveMessage(sqsParams, function(err, data) {
   if (err) {
     console.log('Receive error', err)
   } else {
-    console.log(data.Messages[0].MessageAttributes.source.StringValue) /*
+    var s3Link = data.Messages[0].MessageAttributes.source.StringValue
+    // console.log(data.Messages[0].MessageAttributes.source.StringValue)
+    var s3Params = parseSQS(s3Link)
+    console.log(s3Params)
+    s3.getObject(s3Params, function (err, data) {
+      if(err) {
+        console.log(err)
+      }
+      console.log(data)
+    })
+
+     /*
     var deleteParams = {
       QueueUrl: queueURL,
       ReceiptHandle: data.Messages[0].ReceiptHandle
@@ -36,3 +54,16 @@ sqs.receiveMessage(params, function(err, data) {
     }) */
   }
 })
+	
+function parseSQS(s3link) {
+  var startBucket = 25
+  var bucket = s3link.substr(startBucket, startBucket)
+  var key = s3link.substr(startBucket * 2 + 1, s3link.length - 1)
+
+  var s3parameters = {
+    Bucket: bucket,
+    Key: key
+  }
+
+  return s3parameters
+}
